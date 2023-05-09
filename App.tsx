03bf22efec01917398254e208b2037302e0e63b9
Cauã Silva <117788495+Shisui55@users.noreply.github.com>
2023-05-08 21:23:46 -0300

    Update LABEL App.tsx

--- App.tsx
+++ App.tsx
@@ -1,30 +1,75 @@
-import { Text, View, StyleSheet, TextInput, TouchableOpacity } from 'react-native';
+import React, { useState } from 'react';
+import {
+  Text,
+  View,
+  StyleSheet,
+  TextInput,
+  TouchableOpacity,
+} from 'react-native';
 
 export default function App() {
-  // comentários
+  const [name, setName] = useState('');
+  const [members, setMembers] = useState<string[]>([]);
+
+  function handleSubmit() {
+    setMembers(parms => [name, ...parms]);
+
+    setName('');
+  }
+
   return (
     <View style={styled.container}>
-      <Text key={1}style={styled.title}>
+      <Text key={1} style={styled.title}>
         Nome do evento
       </Text>
+
       <Text key={2} style={styled.subTitle}>
-        Sexta, 4 de Novembro de 2022
+        Sexta, 4 de Novembro de 2022.
       </Text>
-       
-      <View style = {styled.container}>
+
+      <View style={styled.containerInput}>
         <TextInput
           placeholder="Nome do participante"
-          placeholderTextColor='#6B6B6B'
-          style = {styled.input}
+          placeholderTextColor="#6B6B6B"
+          style={styled.input}
+          onChangeText={setName}
+          value={name}
         />
 
-      <TouchableOpacity style = {styled.buttonAdd}{
-        <Text style = {styled.labelButton}></Text>+</text>        
-      </TouchableOpacity>
-  
+        <TouchableOpacity style={styled.buttonAdd} onPress={handleSubmit}>
+          <Text style={styled.labelButton}>+</Text>
+        </TouchableOpacity>
+      </View>
+
+      <Text key={3} style={styled.participantes}>
+              Participantes
+      </Text>
+
+      {/* View da lista de participantes */}
+      <View  style={styled.containerInput}>
+        
+        <Text  style= {styled.labellistaparticipantes}> 
+          Nomes
+        </Text>
+
+        <TouchableOpacity style={styled.buttonRemove} onPress={handleSubmit}>
+            <Text style={styled.labelButton}>-</Text>
+        </TouchableOpacity>
+
+      </View>
+      
+      {members.map((name, index) => {
+        console.log(name + index);
+
+        return (
+          <Text key={name + index} style={styled.subTitle}>
+            {name}
+          </Text>
+        );
+      })}
     </View>
-  </View>
-  ;
+    
+  );
 }
 
 const styled = StyleSheet.create({
@@ -33,32 +78,77 @@ const styled = StyleSheet.create({
     flex: 1,
     paddingHorizontal: 24,
     paddingVertical: 25,
-
   },
-
   title: {
+    color: '#FDFCFE',
+    fontSize: 24,
+    lineHeight: 28.13,
+    fontWeight: '700',
+  },
+  subTitle: {
     color: '#6B6B6B',
-    fontSize: 16,
     lineHeight: 18.75,
+    fontSize: 16,
     fontWeight: '400',
-
-    subTitle: {
-      color: '#FDFCFE',
-      fontSize: 24,
-      lineHeight: 28.13,
-      fontWeight: '700',
-    },
-
+  },
   input: {
-    width: 327,
+    flex: 1,
     height: 56,
     borderRadius: 4,
-    backgroundcolor: '#1F1E25' ,
-    position: abs,
-    left: 24,
-    top: 156,
-    }
+    backgroundColor: '#1F1E25',
+    color: '#FDFCFE',
+    alignItems: 'center',
+    paddingHorizontal: 16,
+    fontSize: 14,
+    lineHeight: 20,
+    fontWeight: '400',
+  },
+  containerInput: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginTop: 24,
+    gap: 7,
+  },
+  buttonAdd: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    width: 56,
+    height: 56,
+    borderRadius: 4,
+    backgroundColor: '#31CF67',
+  },
+  buttonRemove: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    width: 56,
+    height: 56,
+    borderRadius: 4,
+    backgroundColor: 'red',
+  },
+  labelButton: {
+    color: '#fff',
+    fontSize: 24,
+    lineHeight: 18.75,
+    fontWeight: '400',
+  },
+  labellistaparticipantes: {
+    fontSize: 18,
+    lineHeight: 60,
+    flex: 1,
+    height: 60,
+    borderRadius: 4,
+    backgroundColor: '#1F1E25',
+    color: '#FDFCFE',
+    paddingHorizontal: 16,
+    fontWeight: '400',
+    
+  },
+  participantes: {
+    color: '#FDFCFE',
+    fontSize: 24,
+    lineHeight: 28.13,
+    fontWeight: '700',
+    top: 30
+  },
 
-  }
 });
-
